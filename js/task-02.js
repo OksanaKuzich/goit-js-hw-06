929eb4a00143a71ee0fd6f24ad9e02d8fdf2382a
OksanaKuzich <komoks255@gmail.com>
2022-08-19 16:49:03 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const liItems = [];
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+
+  liItems.push(item);
+});
+
+const list = document.querySelector("ul");
+list.append(...liItems);
