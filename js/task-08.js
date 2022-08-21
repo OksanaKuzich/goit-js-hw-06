1b5fb4925dc2df82edebb8e02cb085f36eb1fef7
OksanaKuzich <komoks255@gmail.com>
2022-08-21 17:45:37 +0300

    task 8 and 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault;
+
+  const formDate = event.currentTarget.elements;
+  console.log(formDate);
+
+  if (formDate.email.value === "" || formDate.password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  event.currentTarget.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+btn.addEventListener("click", onClickHandler);
+
+function onClickHandler(event) {
+  const hexColor = getRandomHexColor();
+  spanEl.textContent = hexColor;
+  bodyEl.style.backgroundColor = hexColor;
+}
