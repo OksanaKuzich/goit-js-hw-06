7cd4144c2b931d35b92b5a058e167f76b666db49
OksanaKuzich <komoks255@gmail.com>
2022-08-21 13:19:26 +0300

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector("#name-input");
+const nameEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", (event) => {
+  nameEl.textContent = event.currentTarget.value
+    ? event.currentTarget.value
+    : "Anonymous";
+});
